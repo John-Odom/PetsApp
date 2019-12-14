e31c5ef42c5aaa468a4a17b56664ddc7773a39a5
John Odom <john.osborne.odom@gmail.com>
2019-12-13 19:38:27 -0500

    show page appearing

--- src/pages/dogPage.js
+++ src/pages/dogPage.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+// import '../stylesheets/moviePage.css';
+// import MovieBar from '../moviePage/movieBar';
+// import MovieInfo from '../moviePage/movieInfo';
+// import Sources from '../moviePage/sources';
+// import Trailer from '../moviePage/trailer';
+// import MovieCast from '../moviePage/movieCast';
+import { Container, Divider, Grid } from 'semantic-ui-react';
+// import NavBar from '../navBar'
 
-const dogPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+class DogPage extends Component {
 
-export default dogPage;
+   render() {
+
+      return(
+         <div id="movie-background">
+            {/* <NavBar /> */}
+            <Container id='movie-pg-container'>
+               {/* <MovieBar /> */}
+               <Divider />
+               <Grid columns={2} >
+                  {/* <MovieInfo />
+                  <Sources />
+                  <Trailer />
+                  <MovieCast /> */}
+               </Grid>
+            </Container>
+         </div>
+      )
+   }
+} 
+
+export default withRouter(DogPage);
